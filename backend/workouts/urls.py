07e50324fdccb3f86d9ac960665c0a0bded29254
czarnick89 <nso83@hotmail.com>
2025-08-13 20:38:20 -0500

    changed some views to APIviews

--- backend/workouts/urls.py
+++ backend/workouts/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WorkoutViewSet, ExerciseViewSet, SetViewSet, TemplateSetViewSet, TemplateExerciseViewSet, TemplateWorkoutViewSet, exercise_gif_proxy, exercise_by_name_proxy, google_calendar_auth_start, google_calendar_oauth2callback, add_workout_to_calendar, google_calendar_status, google_calendar_disconnect
+from .views import (
+    WorkoutViewSet,
+    ExerciseViewSet,
+    SetViewSet,
+    TemplateSetViewSet,
+    TemplateExerciseViewSet,
+    TemplateWorkoutViewSet,
+    ExerciseGifProxy,
+    ExerciseByNameProxy,
+    GoogleCalendarAuthStart,
+    GoogleCalendarOAuth2Callback,
+    AddWorkoutToCalendar,
+    GoogleCalendarStatus,
+    GoogleCalendarDisconnect,
+)
 
 router = DefaultRouter()
 router.register('workouts', WorkoutViewSet, basename='workout')
@@ -12,12 +26,11 @@ router.register('template-sets', TemplateSetViewSet, basename='templateset')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('exercise-gif/', exercise_gif_proxy, name='exercise_gif_proxy'),
-    path('exercise-by-name/', exercise_by_name_proxy, name = 'exercise_by_name_proxy'),
-    path('google-calendar/auth-start/', google_calendar_auth_start, name = 'google_calendar_auth_start'),
-    path('google-calendar/oauth2callback', google_calendar_oauth2callback, name='google_calendar_oauth2callback'),
-    path('google-calendar/create-event/', add_workout_to_calendar, name='add_workout_to_calendar'),
-    path('google-calendar/status/', google_calendar_status, name='google_calendar_status'),
-    path('google-calendar/disconnect/', google_calendar_disconnect, name='google_calendar_disconnect'),
-    
+    path('exercise-gif/', ExerciseGifProxy.as_view(), name='exercise_gif_proxy'),
+    path('exercise-by-name/', ExerciseByNameProxy.as_view(), name='exercise_by_name_proxy'),
+    path('google-calendar/auth-start/', GoogleCalendarAuthStart.as_view(), name='google_calendar_auth_start'),
+    path('google-calendar/oauth2callback/', GoogleCalendarOAuth2Callback.as_view(), name='google_calendar_oauth2callback'),
+    path('google-calendar/create-event/', AddWorkoutToCalendar.as_view(), name='add_workout_to_calendar'),
+    path('google-calendar/status/', GoogleCalendarStatus.as_view(), name='google_calendar_status'),
+    path('google-calendar/disconnect/', GoogleCalendarDisconnect.as_view(), name='google_calendar_disconnect'),
 ]
